a0d44ef632e3119ec31451a3698a21171ce7c216
bistouri <auguste.elengesa@gmail.com>
2025-04-08 10:54:26 +0200

    Moving slot machine activity file

--- app/src/main/java/com/example/myapplication/slotmachine/SlotMachineActivity.kt
+++ app/src/main/java/com/example/myapplication/slotmachine/SlotMachineActivity.kt
@@ -1,8 +1,7 @@
-package com.example.myapplication
+package com.example.myapplication.slotmachine
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -11,8 +10,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.myapplication.slotmachine.Fruit
-import com.example.myapplication.slotmachine.SlotMachine
+import com.example.myapplication.BanqueActivity
+import com.example.myapplication.R
 
 class SlotMachineActivity : AppCompatActivity() {
     val machine: SlotMachine = SlotMachine(luck = 0.3F)
